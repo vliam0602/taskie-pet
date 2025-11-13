73731bbf128c10ef40f0e533a747a7a065dd4983
Lam Vo <v.trclam@gmail.com>
2025-11-13 17:41:56 +0700

    add login form ui

---
+++ TaskiePet.Client/src/pages/Login.tsx
@@ -0,0 +1,87 @@
+import Logo from "../assets/pet-paw-logo.png";
+export default function Login() {
+	return (
+		<>
+			<div className="flex w-100 min-h-full flex-col px-6 py-12 lg:px-8">
+				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+					<img
+						alt="Logo"
+						src={Logo}
+						className="mx-auto h-20 w-auto"
+					/>
+					<h1 className="">Taskie Pet</h1>
+					<h2 className="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+						Sign in
+					</h2>
+				</div>
+
+				<div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+					<form action="#" method="POST" className="space-y-6 ">
+						<div id="input-email-container">
+							<label className="input validator">
+								<svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<g
+										strokeLinejoin="round"
+										strokeLinecap="round"
+										strokeWidth="2.5"
+										fill="none"
+										stroke="currentColor"
+									>
+										<rect width="20" height="16" x="2" y="4" rx="2"></rect>
+										<path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+									</g>
+								</svg>
+								<input type="email" placeholder="mail@site.com" required />
+							</label>
+							<div className="validator-hint hidden text-left">Enter valid email address</div>
+						</div>
+
+						<div id="input-password-container">
+							<label className="input validator">
+								<svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<g
+										strokeLinejoin="round"
+										strokeLinecap="round"
+										strokeWidth="2.5"
+										fill="none"
+										stroke="currentColor"
+									>
+										<path
+											d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+										></path>
+										<circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+									</g>
+								</svg>
+								<input
+									type="password"
+									required
+									placeholder="Password"
+									minLength={6}
+									pattern="(?=.*\d)(?=.*[a-z]).{6,}"
+									title="Must be more than 6 characters, including number, lowercase letter"
+								/>
+							</label>
+							<p className="validator-hint hidden text-left">
+								Must be more than 6 characters and
+								<br />including at least one number
+							</p>
+						</div>
+
+						<div id="action-btn-container" className="flex flex-col gap-4">
+							<button className="btn w-full bg-white text-black border-[#e5e5e5]"
+								type="submit">
+								<svg aria-label="Email icon" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="black"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></g></svg>
+								Login with Email
+							</button>
+							<p>--- or ---</p>
+							<button className="btn w-full bg-white text-black border-[#e5e5e5]">
+								<svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+								Login with Google
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</>
+	)
+}
